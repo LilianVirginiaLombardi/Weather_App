493abae6df7463563a8d97f227278aa908acd36a
Lilian Virginia Lombardi López <virginia.lombardi@globant.com>
2022-06-27 15:18:49 -0300

    Base Project

---
+++ Weather_App/Weather_App/Weather_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weather_App
+//
+//  Created by Virginia Lombardi Lopez on 27/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
